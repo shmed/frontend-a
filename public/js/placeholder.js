5d97ae75daddace8e2c7062e04604c903ded020f
Raouf Merouche <ashmed@gmail.com>
2014-05-06 20:38:42 -0400

    Fix some css issue for better legacy browser support

---
+++ public/js/placeholder.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+	setPlaceHolder('from-search');
+	setPlaceHolder('to-search');
+});
+
+function setPlaceHolder(id){
+	$('#' + id).focus(function() {
+	  var input = $(this);
+	  if (input.val() == input.attr('placeholder')) {
+	    input.val('');
+	    input.removeClass('placeholder');
+	  }
+	}).blur(function() {
+	  var input = $(this);
+	  if (input.val() == '' || input.val() == input.attr('placeholder')) {
+	    input.addClass('placeholder');
+	    input.val(input.attr('placeholder'));
+	  }
+	}).blur();
+	
+}
